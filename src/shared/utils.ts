72e22d43936f3bfb1f8a1dd25c9838864aaeb8fd
Cristóbal Contreras Rubio <crisconru@gmail.com>
2023-06-19 13:48:16 +0200

    adding functions and tested everything

--- src/shared/utils.ts
+++ src/shared/utils.ts
@@ -6,3 +6,5 @@ export const computedCRC = (data: Buffer): number => crc16xmodem(data)
 export const bitState = (num: number, bit: number): boolean => (num >>> bit) % 2 != 0
 
 export const getPadding = (data: Buffer, index: number = 0, length: number = 0): Padding => (length > 0) ? data.readUIntLE(index, length): null
+
+export const getNullableValue = (value: any, callback: CallableFunction) => (value !== null) ? callback(value) : null
